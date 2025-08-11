1e3140a1b4d716e26cd4cb4c30fe092ebe68d6f2
Ryder Brown <bassmasta25@hotmail.com>
2025-08-10 23:02:01 -0700

    Move Health PU animation to Animator
    Removed scripts handling the little health animation after picking up a health pick up. Moved the animation to the animator and created a clip with animation controller.

--- Assets/Scripts/Animation/FloatingHealthParent.cs
+++ Assets/Scripts/Animation/FloatingHealthParent.cs
@@ -2,27 +2,5 @@ using UnityEngine;
 
 public class FloatingHealthParent : MonoBehaviour
 {
-    [SerializeField] private HealthPickUpAnimation[] childObjects;
 
-    private int counter = 0;
-
-    public void OnChildFadeComplete()
-    {
-        counter++;
-
-        if (counter >= childObjects.Length)
-        {
-            counter = 0;
-            ResetChildren();
-            gameObject.SetActive(false);
-        }
-    }
-
-    private void ResetChildren()
-    {
-        foreach (var child in childObjects)
-        {
-            child.Reset();
-        }
-    }
 }
