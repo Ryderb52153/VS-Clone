804dd659b2fb6887c9e65bead88f807d2d76505e
Ryder Brown <bassmasta25@hotmail.com>
2025-08-24 19:46:51 -0700

    Added default cursor
    Default cursor added

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class CursorManager : MonoBehaviour
 {
     [SerializeField] private Texture2D cursorTexture;  
+    [SerializeField] private Texture2D cursorDefault;
     [SerializeField] private Vector2 clickPoint = Vector2.zero; 
     [SerializeField] private CursorMode cursorMode = CursorMode.Auto;
 
-    void Start()
+    private void Start()
+    {
+        SetDefaultCursor();
+    }
+
+
+    public void SetDefaultCursor()
+    {
+        Cursor.SetCursor (cursorDefault, clickPoint, cursorMode);
+    }
+
+    public void SetTargetCursor()
     {
         Cursor.SetCursor(cursorTexture, clickPoint, cursorMode);
     }
-
 }
