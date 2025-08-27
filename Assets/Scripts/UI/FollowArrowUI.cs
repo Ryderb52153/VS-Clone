592169f69dc869fe783c8000e4d15bdbf796a3f5
Ryder Brown <bassmasta25@hotmail.com>
2025-08-27 00:31:45 -0700

    Treasure Chest/Follow Arrow UI
    Added a treasure chest sprite in the game, then added a UI arrow image that will point towards the object getting the distance between the player and the target object. Need to add functionality of treasure, maybe require certain level, and then some way to open it. Also add a way to change target of the arrow.

---
+++ Assets/Scripts/UI/FollowArrowUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CompassArrow : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private RectTransform arrowUI; // your UI Image/RectTransform
+    [SerializeField] private Transform player;      // player/world origin
+    [SerializeField] private Transform target;      // destination (e.g., chest)
+
+
+    private void Update()
+    {
+        Vector3 v = target.position - player.position;
+        arrowUI.eulerAngles = new Vector3(0, 0, Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg);
+    }
+}
