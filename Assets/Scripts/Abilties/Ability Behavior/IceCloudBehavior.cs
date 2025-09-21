256486868886fe64206633cbb848dce171550eb7
Ryder Brown <bassmasta25@hotmail.com>
2025-09-21 02:11:18 -0700

    Added Folders/ Updated Ice Cloud
    Added folders to organize scripts and images. Updated Ice cloud lifespan to work properly. Will update ice cloud to be more of a particle effect later

---
+++ Assets/Scripts/Abilties/Ability Behavior/IceCloudBehavior.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class IceCloudBehavior : MonoBehaviour
+{
+    private float timeRemaining;
+    private float lifeSpan = 3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") { return; }
+
+        if (collision.TryGetComponent<ItakeDamage>(out ItakeDamage hitCollider))
+        {
+            hitCollider.Slow(1, 2f);
+        }
+    }
+
+    private void OnTick(object sender, TimeTickSystem.OnTickEventArgs e)
+    {
+        timeRemaining--;
+
+        if (timeRemaining < 0)
+        {
+            timeRemaining = lifeSpan;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        TimeTickSystem.OnTick += OnTick;
+        timeRemaining = lifeSpan;
+    }
+
+    private void OnDisable()
+    {
+        TimeTickSystem.OnTick -= OnTick;
+    }
+}
